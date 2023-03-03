4c6e6109fba8753e0ee7de2742c7d63e915cd4c3
KxmischesDomi <kxmischesdomi@gmail.com>
2023-03-03 20:19:00 +0100

    some changes bruh

---
+++ static/src/item.js
@@ -0,0 +1,64 @@
+
+function injectShopItemContent(title, price, image, afterInject = () => {}) {
+    injectShopItemStyle();
+    fetch("/templates/item.html")
+        .then(response => response.text())
+        .then(text => {
+            text = text.replaceAll("%title%", title)
+                .replaceAll("%price%", price)
+                .replaceAll("%image%", image);
+            document.getElementById("inject_content").innerHTML = text;
+            injectSmallImage(image, true);
+            afterInject();
+        });
+}
+
+function injectSmallImage(image, defaultEnabled = false) {
+    classes = "small_image" + (defaultEnabled ? " active" : "");
+    document.getElementById("inject_small_images").insertAdjacentHTML("beforeend", `<button class="${classes}" onclick="onSmallImageClick(this)" style="background-image: url(${image});"></button>`);
+}
+
+function injectSizeSetting() {
+    injectSetting(`
+         <label>Größe*</label>
+         <select id="setting_size">
+          <option>Größe Auswählen</option>
+          <option value="s">S</option>
+          <option value="m">M</option>
+          <option value="l">L</option>
+          <option value="xl">XL</option>
+          <option value="xxl">XXL</option>
+          <option value="3xl">3XL</option>
+          <option value="4xl">4XL</option>
+          <option value="5xl">5XL</option>
+        </select>
+    `)
+}
+
+function injectSetting(option) {
+    document.getElementById("inject_settings").insertAdjacentHTML("afterbegin", `${option}`)
+}
+
+function injectShopItemStyle() {
+    const style = document.createElement("link");
+    style.type = "text/css"
+    style.rel = "stylesheet"
+    style.href = "/styles/item.css"
+    document.head.appendChild(style);
+}
+
+// End Injecting
+// Start Functionality
+
+function onSmallImageClick(imageElement) {
+
+    var children = document.getElementById("inject_small_images").children;
+    for (let i = 0; i < children.length; i++) {
+        var child = children[i];
+        child.classList.remove("active");
+    }
+
+    document.getElementById("inject_big_image_src").style.backgroundImage = imageElement.style.backgroundImage;
+    imageElement.classList.add("active");
+}
+
